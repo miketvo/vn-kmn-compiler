adc4189668c8a420028e5a3b1d11047dcf298741
miketvo <ikxplicitie@gmail.com>
2022-03-01 13:24:32 +0700

    Finished

--- vn_telex/compile.py
+++ vn_telex/compile.py
@@ -6,7 +6,6 @@ import vn_telex.utils.vnrhymes as vnr
 
 HEADER_PATH = './raw/header.kmn'
 OUT_PATH = './compiled/out.kmn'
-MODIFIERS = list('sfrxjwoa')
 
 
 def main():
@@ -22,16 +21,22 @@ def main():
         permutation_matrix = charcases.gen_case_permutations_matrix(len(rhymes[i].base))
         for permutation_line in permutation_matrix:
             base = charcases.apply_case(rhymes[i].base, permutation_line)
-            result = charcases.apply_case(rhymes[i].result, permutation_line)
-            rhymes_cases.append(vnr.TelexRhyme(base, rhymes[i].modifier.lower(), result))
-            rhymes_cases.append(vnr.TelexRhyme(base, rhymes[i].modifier.upper(), result))
+            if len(rhymes[i].result) == len(permutation_line):
+                result = charcases.apply_case(rhymes[i].result, permutation_line)
+                rhymes_cases.append(vnr.TelexRhyme(base, rhymes[i].modifier.lower(), result))
+                rhymes_cases.append(vnr.TelexRhyme(base, rhymes[i].modifier.upper(), result))
+            elif len(rhymes[i].result) == len(permutation_line) + 1:
+                result = charcases.apply_case(rhymes[i].result[0:-1], permutation_line) + rhymes[i].modifier.lower()
+                rhymes_cases.append(vnr.TelexRhyme(base, rhymes[i].modifier.lower(), result))
+                result = charcases.apply_case(rhymes[i].result[0:-1], permutation_line) + rhymes[i].modifier.upper()
+                rhymes_cases.append(vnr.TelexRhyme(base, rhymes[i].modifier.upper(), result))
             progbar.print_bar(
                 percentage=round(i / len(rhymes) * 100),
                 message=f'({i}/{len(rhymes)}) Processing {rhymes[i].result}'
             )
-    progbar.print_done(f'{len(rhymes_cases)} generated. [DONE]')
+    progbar.print_done(f'Generating uppercase and lowercase permutations... {len(rhymes_cases)} generated. [DONE]')
 
-    print('Generating Keyman code... ')
+    print('Generating Keyman code... ', end='')
     content = []
     for i in range(len(rhymes_cases)):
         code = f"    '{rhymes_cases[i].base}' + '{rhymes_cases[i].modifier}' > '{rhymes_cases[i].result}'\n"
@@ -40,9 +45,9 @@ def main():
             percentage=round(i / len(rhymes_cases) * 100),
             message=f'({i}/{len(rhymes_cases)}) {code[:-1]}'
         )
-    progbar.print_done(f'Code generated.')
+    progbar.print_done(f'Generating Keyman code... {len(content)} line(s) generated.')
 
-    print('Saving Keyman code... ', end='')
+    print(f'Saving Keyman code at {OUT_PATH} ... ', end='')
     header = []
     headerf = open(HEADER_PATH, 'r', encoding='utf-8')
     for line in headerf:
